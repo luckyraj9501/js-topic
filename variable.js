a146bfda3680bc08e5f8de44590293536a0fe0ea
Rohit Raj <32592866+luckyraj9501@users.noreply.github.com>
2023-10-11 16:15:08 +0000

    variable topic session

---
+++ variable.js
@@ -0,0 +1,9 @@
+const accountNumber = 784785
+// const accountNumber = 4545454 //cannot change value and redeclare
+let name = "Rohit Raj"
+// let name = "Kundan Kumar" // value can change but cannot redclare
+name = "Kundan Kumar"
+var age = 45
+// var age = 58 // calue can change and redclared
+test = "without variable"
+console.table([accountNumber,name,age,test])
