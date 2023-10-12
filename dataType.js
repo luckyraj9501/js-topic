527faa816453b9ce3813361c5676a89ff5b2f8da
Rohit Raj <32592866+luckyraj9501@users.noreply.github.com>
2023-10-12 07:06:10 +0000

    dataType

---
+++ dataType.js
@@ -0,0 +1,11 @@
+// number => 2 power 53
+//bitint
+//string => ""
+//boolean => true/false
+//null => standlone value
+//undefined => a varaiable declared without any value
+//symbol => unique
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
