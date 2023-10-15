545c96887a56314dd73178cbb908b33f743ca1b5
Rohit Raj <32592866+luckyraj9501@users.noreply.github.com>
2023-10-15 17:13:50 +0000

    conversion of data types

---
+++ conversionOperation.js
@@ -0,0 +1,16 @@
+let score = 55;
+console.log(typeof(score)); //output: number
+
+let score2 = "rohit";
+console.log(typeof(score2)); //output: string
+
+let valueInnumber = Number(score2)
+console.log(typeof (valueInnumber)); //output: number
+
+let isLogin = "rohit";
+let booleanIsLogin = Boolean(isLogin);
+console.log(booleanIsLogin); //output: true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof (stringNumber)); //output: string
